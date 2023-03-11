cf93677f8052a88e8c0fdd9cf5358c2fa3dd5253
mosesnjoroge <mosesbnjoroge@outlook.com>
2023-03-11 15:22:46 -0500

    addition of bootstrap and cloudinary api library

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import './App.css';
+import Nav from './components/Navbar';
+import Entries from './components/Entries';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
   return (
     <div className="App">
-
+      <Nav/>
+      <Entries/>
     </div>
   );
 }
