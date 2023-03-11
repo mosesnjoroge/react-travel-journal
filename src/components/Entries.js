bc1f6963bb5913b13b56dfb11d549aa2b723dc05
mosesnjoroge <mosesbnjoroge@outlook.com>
2023-03-11 15:32:24 -0500

    entries component cl setup

--- src/components/Entries.js
+++ src/components/Entries.js
@@ -1,10 +1,24 @@
 import React from "react";
-
+import {AdvancedImage} from '@cloudinary/react';
+import {Cloudinary} from "@cloudinary/url-gen";
+import {fill} from "@cloudinary/url-gen/actions/resize";
 
 export default function Entries() {
 
-  return  (
+  const cld = new Cloudinary({
+    cloud: {
+      cloudname: 'demo'
+    }
+  });
 
+  const myImage = cld.image('docs/models');
+
+  myImage.resize(fill().width(250).height(250));
+
+  return  (
+    <div>
+      <AdvancedImage cldImg={myImage} />
+    </div>
 
   );
 }
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,10 +1,15 @@
 import React from "react";
+import { Navbar } from "react-bootstrap";
 
 
 export default function Nav() {
 
   return  (
+    <div>
+      <Navbar>
 
 
+      </Navbar>
+    </div>
   );
 }
