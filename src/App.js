57f5c1a17845be7a3c1f96ac198b07ca2d4f57e6
mosesnjoroge <mosesbnjoroge@outlook.com>
2023-03-11 20:02:56 -0500

    adding map method and creating a data file to transform elements instead of hard coding

--- src/App.js
+++ src/App.js
@@ -3,32 +3,16 @@ import Nav from './components/Nav';
 import Entries from './components/Entries';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './components/stylesheets/Cards.css';
+import entryData from './entryData'
 
 function App() {
-
+  const entryElements = entryData.map(entry => {
+    return <Entries posttitle={entry.posttitle} entrytext={entry.entrytext} />
+  })
   return (
     <div className="App">
       <Nav/>
-
-      <Entries
-        posttitle = 'cuba'
-        entrytext = 'very cool place'
-      />
-
-      <Entries
-        posttitle = 'cuba'
-        entrytext = 'very cool place'
-      />
-
-      <Entries
-        posttitle = 'cuba'
-        entrytext = 'very cool place'
-      />
-
-      <Entries
-        posttitle = 'cuba'
-        entrytext = 'very cool place'
-      />
+      {entryElements}
     </div>
   );
 }
---
+++ src/entryData.js
@@ -0,0 +1,22 @@
+export default [
+  {
+    posttitle: 'cuba',
+    entrytext: 'very cool place'
+  },
+  {
+    posttitle: 'cuba',
+    entrytext: 'very cool place'
+  },
+  {
+    posttitle: 'cuba',
+    entrytext: 'very cool place'
+  },
+  {
+    posttitle: 'cuba',
+    entrytext: 'very cool place'
+  },
+  {
+    posttitle: 'cuba',
+    entrytext: 'very cool place'
+  }
+]
