ad31a5dc57ac63c1a7de41408e10350ec3d45fcf
mosesnjoroge <mosesbnjoroge@outlook.com>
2023-03-11 20:40:29 -0500

    data entry file syntax cleanup

--- src/App.js
+++ src/App.js
@@ -3,11 +3,15 @@ import Nav from './components/Nav';
 import Entries from './components/Entries';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './components/stylesheets/Cards.css';
-import entryData from './entryData'
+import data from './data'
 
 function App() {
-  const entryElements = entryData.map(entry => {
-    return <Entries posttitle={entry.posttitle} entrytext={entry.entrytext} />
+  const entryElements = data.map(entry => {
+    return <Entries
+    id = {entry.id}
+    posttitle = {entry.posttitle}
+    entrytext = {entry.entrytext}
+     />
   })
   return (
     <div className="App">
---
+++ src/data.js
@@ -0,0 +1,28 @@
+export default [
+
+    {
+      id: 1,
+      posttitle: 'cuba',
+      entrytext: 'very cool place'
+    },
+    {
+      id: 2,
+      posttitle: 'dr',
+      entrytext: 'very cool place'
+    },
+    {
+      id: 3,
+      posttitle: 'cuba',
+      entrytext: 'very cool place'
+    },
+    {
+      id: 4,
+      posttitle: 'cuba',
+      entrytext: 'very cool place'
+    },
+    {
+      id: 5,
+      posttitle: 'cuba',
+      entrytext: 'very cool place'
+    }
+]
